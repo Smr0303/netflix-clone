d0ce142c66311ee933da4c36947da514979ca6ae
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-14 00:07:27 +0530

    Sliding rows

--- src/Row.js
+++ src/Row.js
@@ -3,7 +3,7 @@ import axios from "./axios";
 import "./Row.css";
 const base_url = "https://image.tmdb.org/t/p/original";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl ,isLarge}) {
   const [movies, setmovies] = useState([]);
   useEffect(() => {
     async function fetchData() {
@@ -15,14 +15,14 @@ function Row({ title, fetchUrl }) {
 
   return (
     <div className="row">
-      <h1> {title}</h1>;
-      <div className="row_posters">
+      <h1> {title}</h1>
+      <div className={isLarge?`row_posters row_posterLarge`:`row_posters`}>
         {console.log(movies.length)}
         {movies.map((movie) => {
           return (
             <img key={movie.id}
               className="row_poster"
-              src={`${base_url}${movie.poster_path}`}
+              src={`${base_url}${isLarge?movie.poster_path:movie.backdrop_path}`}
               alt={movie.title}
             />
           );
