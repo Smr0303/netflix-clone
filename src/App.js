00a2b0aafafcf854eb0cdcc937604a5689c1c66d
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-14 11:55:37 +0530

    Banner added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './App.css'; 
 import Row from './Row';
 import requests from './requests';
+import Banner from './Banner';
 
 function App() {
   return (
     <div className='App'>
+ <Banner/>
+
+
     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLarge={true}/>
     <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
