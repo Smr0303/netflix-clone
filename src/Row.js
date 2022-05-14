3e06b77998ad8ca3805500fadfd2375c5a6caa20
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-14 15:17:53 +0530

    Banner done

--- src/Row.js
+++ src/Row.js
@@ -3,7 +3,7 @@ import axios from "./axios";
 import "./Row.css";
 const base_url = "https://image.tmdb.org/t/p/original";
 
-function Row({ title, fetchUrl ,isLarge}) {
+function Row({ title, fetchUrl, isLarge }) {
   const [movies, setmovies] = useState([]);
   useEffect(() => {
     async function fetchData() {
@@ -17,12 +17,14 @@ function Row({ title, fetchUrl ,isLarge}) {
     <div className="row">
       <h1> {title}</h1>
       <div className="row_posters">
-        {console.log(movies.length)}
         {movies.map((movie) => {
           return (
-            <img key={movie.id}
-              className={isLarge?"row_poster row_posterLarge":"row_poster "}
-              src={`${base_url}${isLarge?movie.poster_path:movie.backdrop_path}`}
+            <img
+              key={movie.id}
+              className={isLarge ? "row_poster row_posterLarge" : "row_poster "}
+              src={`${base_url}${
+                isLarge ? movie.poster_path : movie.backdrop_path
+              }`}
               alt={movie.title}
             />
           );
