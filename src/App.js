635e6f1908477dcc00492385385a07e5700f0d88
Samarjeet Mohite <samarmohite7@gmail.com>
2022-05-15 14:43:59 +0530

    NavBar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import Row from './Row';
 import requests from './requests';
 import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className='App'>
+      <Nav/>
+
     <Banner/>
     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLarge={true}/>
     <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import './Nav.css';
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <div className='Nav'>
+    <img className='Nav_logo' src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Netflix_2014_logo.svg" alt="Netflix Logo"/>
+    <img className='Nav-avatar' src="https://upload.wikimedia.org/wikipedia/commons/5/54/Netflix_logo.png" alt="Avatar"/>
+    </div>
   )
 }
 
